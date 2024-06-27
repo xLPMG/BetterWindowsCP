789dce41eee24f6633620e85f5d28bd26cde819c
Luca-Philipp Grumbach <xxlpmghdxx@gmail.com>
2024-06-27 23:21:52 +0200

    Revert "PR to sync folder structure (#7)" (#9)
    This reverts commit 1c642e0fce99e7f00dfae16126aea3fe24779443.

--- macCP-A.sh
+++ macCP-A.sh
@@ -1,1 +1,1 @@
-yes | cp -rf "[CP-A] BetterWindowsCP-A" "/Users/lpmg/Library/Application Support/Steam/steamapps/common/Stardew Valley/Contents/MacOS/mods/"
+yes | cp -rf "[CP-A] BetterWindowsCP" "/Users/lpmg/Library/Application Support/Steam/steamapps/common/Stardew Valley/Contents/MacOS/mods/"
